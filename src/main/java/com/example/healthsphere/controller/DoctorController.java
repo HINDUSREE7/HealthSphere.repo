23baddbba2dbc6cdc9d6608d708997b79d809cbc
Hindusree Pillari <pillarihindusree6@gmail.com>
2025-12-08 23:00:57 +0530

    Delete src directory

--- src/main/java/com/example/healthsphere/controller/DoctorController.java
+++
@@ -1,4 +0,0 @@
-package com.example.healthsphere.controller;
-
-public class DoctorController {
-}
--- src/main/java/com/example/healthsphere/security/JwtAuthTokenFilter.java
+++
@@ -1,4 +0,0 @@
-package com.example.healthsphere.security;
-
-public class JwtAuthTokenFilter {
-}
